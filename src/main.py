2e6df89b8c58f906057739524b2e231348a82c2b
jorickdefraine <defraine.jorick@gmail.com>
2020-09-03 16:59:00 +0200

    final submission in sketch/

--- src/main.py
+++ src/main.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     #df = dataloader()
     #print(df)
     #print(sum())
-    #correlation()
-    print(pcanalysis())
+    correlation()
+    #print(pcanalysis())
--- src/vizualisation.py
+++ src/vizualisation.py
@@ -5,28 +5,31 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 
-def dataloader():
+def dataLoader():
     raw_df = pd.read_csv('./dataset/train.csv')
-    #print(raw_df.head())
-    #print(raw_df.loc[:, raw_df.columns != 'Id'])
-    clean_df = raw_df[raw_df.bare_nuclei>=1]
+    # print(raw_df.head())
+    # print(raw_df.loc[:, raw_df.columns != 'Id'])
+    clean_df = raw_df[raw_df.bare_nuclei >= 1]
     clean_df = clean_df.reset_index(drop=True)
     return clean_df
 
+
 def correlation():
-    correlation = dataloader(). drop('Id', axis=1).corr(method='pearson')
+    correlation = dataLoader().drop('Id', axis=1).corr(method='pearson')
     sns.heatmap(correlation, xticklabels=correlation.columns, yticklabels=correlation.columns)
     plt.title("Correlation Matrix")
     plt.show()
 
+
 def sum():
-    df = dataloader()
+    df = dataLoader()
     sum = df.loc[df['class'] == 4].sum()
 
     return sum
 
-def pcanalysis():
-    df = dataloader()
+
+def pCAnalysis():
+    df = dataLoader()
     df = df.loc[:, df.columns != 'Id']
     pca = PCA(n_components=1)
     pca.fit(df)
